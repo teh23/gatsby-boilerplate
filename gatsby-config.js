60dabce228aff0b9f9804c82a20860f932cf914c
Andrzej Franek <tehix1@gmail.com>
2021-12-01 15:20:02 +0100

    init gatsby & tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     title: "gatsby-boilerplate",
   },
   plugins: [
+    "gatsby-plugin-postcss",
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+// markup
+const IndexPage = () => (
+  <main>
+    Hello, <span className={'font-bold'}>World!</span>
+  </main>
+);
+
+export default IndexPage;
