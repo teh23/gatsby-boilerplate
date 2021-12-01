e24b7aa5f5c6d2dc5db8015526701a72f1dbcdc3
Andrzej Franek <tehix1@gmail.com>
2021-12-01 16:55:23 +0100

    init prettier; init eslint; init typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,23 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-boilerplate",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'gatsby-boilerplate',
   },
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
-};
+}
