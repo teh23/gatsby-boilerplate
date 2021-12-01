63bacded4a805f7049d520ebbf337351b0ba92d5
Andrzej Franek <tehix1@gmail.com>
2021-12-01 20:56:51 +0100

    add eslint-disable to gatsby files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,11 @@
 /* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+
 import './src/styles/global.scss'
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from './src/store'
+import { store } from './src/store'
 
 export const wrapRootElement = ({ element }) => {
   return <Provider store={store}>{element}</Provider>
